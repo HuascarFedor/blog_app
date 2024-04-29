dbe79369eff39184c382a8e6fe6232cc27ec1f22
Huascar <huascarfedor@gmail.com>
2024-04-29 10:18:43 -0400

    tercer commit

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -16,4 +16,17 @@ export class User {
     type: 'varchar',
   })
   password: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updated_at: Date;
 }
